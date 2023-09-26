12978a3aebfde5dbbb2333b694475fe37bb68006
V|ka <qianna@yandex.ru>
2023-09-27 00:40:36 +0300

    main_svc (ТЗ 17.2) v1.23

--- stats-service/stats-server/src/main/java/ru/practicum/stats/StatsServerApp.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/StatsServerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServer {
+public class StatsServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(StatsServer.class, args);
+        SpringApplication.run(StatsServerApp.class, args);
     }
 
 }
