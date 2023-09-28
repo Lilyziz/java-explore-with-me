c38e32255eb1a942c0ba78b05aa0afe1f9fd2cd0
V|ka <qianna@yandex.ru>
2023-09-29 00:53:44 +0300

    main_svc (ТЗ 17.2) v4.19

--- stats-service/stats-server/src/main/java/ru/practicum/stats/service/HitServiceImpl.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/service/HitServiceImpl.java
@@ -13,6 +13,7 @@ import ru.practicum.stats.repository.HitRepository;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
