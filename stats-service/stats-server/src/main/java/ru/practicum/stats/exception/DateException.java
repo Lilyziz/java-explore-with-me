edc1e9b300411b806ffe3fc181d20bec8217ad21
V|ka <qianna@yandex.ru>
2023-09-28 12:48:13 +0300

    main_svc (ТЗ 17.2) v2.13

--- stats-service/stats-server/src/main/java/ru/practicum/stats/exception/DateException.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/exception/DateException.java
@@ -1,6 +1,9 @@
 package ru.practicum.stats.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class DateException extends RuntimeException {
     public DateException(String message) {
         super(message);
